76def3960fe6d8c102cf2fd46096bdf1ed530228
Mateus Devino <mdevino@ibm.com>
2022-02-16 16:10:48 -0300

    Customer Controller

---
+++ src/main/java/com/mdevino/learning/spring/graphql/demo/CustomerController.java
@@ -0,0 +1,20 @@
+package com.mdevino.learning.spring.graphql.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import reactor.core.publisher.Flux;
+
+@Controller
+public class CustomerController {
+	
+	@Autowired
+	private CustomerRepository repository;
+
+	@SchemaMapping(typeName = "Query", field = "customers")
+	Flux<Customer> customers() {
+		return this.repository.findAll();
+	}
+	
+}
